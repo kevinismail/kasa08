074ca90b7d03f5caa8cf072f29fe7305703890ce
Kévin Ismail <kevin.ismails3@gmail.com>
2024-02-29 22:25:04 +0100

    creation d'un fichier json pour le about et utilisation de map, reste a faire les collapse et stars dans un composant

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,5 +1,6 @@
 import Collapse from '../../components/Collapse'
 import aboutImg from '../../data/mountains.jpg'
+import aboutData from '../../data/about.json'
 
 export default function About() {
     return (
@@ -8,22 +9,13 @@ export default function About() {
                 <img src={aboutImg} alt='montagnes' className='About__banner_img' />
             </div>
             <div className="About__collapse-container">
-                <Collapse
-                    title="Fiabilité"
-                    content="Les annonces postées sur Kasa garantissent une fiablité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-                />
-                <Collapse
-                    title="Respect"
-                    content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-                />
-                <Collapse
-                    title="Service"
-                    content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-                />
-                <Collapse
-                    title="Sécurité"
-                    content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-                />
+            {aboutData.map((item, index) => (
+                    <Collapse
+                        key={index} 
+                        title={item.headline}
+                        content={item.content}
+                    />
+                ))}
             </div>
         </div>
     );
