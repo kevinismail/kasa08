1353d225e758aa8a72c44e284d77406d27ea573c
Kévin Ismail <kevin.ismails3@gmail.com>
2024-03-01 17:17:40 +0100

    reprise ancien code collapse et suppression background collapse pour ajout seulement en visible

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,40 +1,26 @@
 import collArrow from '../../data/arrow_collapse.png';
 import { useState } from 'react';
 
-/*export default function Collapse({ title, content }) {
+export default function Collapse({ title, content }) {
 
-    const [toggle, setToggle] = useState(false); */
-
-const Collapse = ({ title, content }) => {
-  const [isCollapsed, setIsCollapsed] = useState(true);
- 
-  const toggleCollapse = () => {
-     setIsCollapsed(!isCollapsed);
-  };
+    const [toggle, setToggle] = useState(false);
 
     return (
-        //<div className="Collapse" >//
-        <div className={`Collapse ${isCollapsed ? "" : "Collapse--active"}`}>
-
-            <h3 className='Collapse__title' onClick={toggleCollapse}>  
+        <div className="Collapse" >
+            <h3 className='Collapse__title'>
                 {title}
                 <img
-                 /*   onClick={() => setToggle(!toggle)}
+                    onClick={() => setToggle(!toggle)}
                     className={toggle ? 'Arrow Arrow__up' : 'Arrow Arrow__down'}
                     src={collArrow}
-                    alt="contenu visible"*/
-                    className="Collapse__icon"
-                    src={collArrow}
-                    alt="Flèche"
-                  
+                    alt="contenu visible"
                 />
             </h3>
-            <div className={`Collapse__content ${isCollapsed ? "Collapse__content_hidden" : ""}`}>
-        {Array.isArray(content) ? content.map((item, index) => {
-          return (<div key={index}>{item}</div>)
-        }) : content}
+            <div className={toggle ? 'Collapse__content' : 'Collapse__content_hidden'}>
+                {Array.isArray(content) ? content.map((item, index) => {
+                    return (<div key={index}>{item}</div>)
+                }) : content}
             </div>
         </div>
     );
 }
-export default Collapse;
